b7d38b89f5f9f1a29230a2d984503d4fee9b4833
Michiel Borkent <michielborkent@gmail.com>
2012-09-01 22:26:30 +0200

    initial commit

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.clojars.borkdude/twitter-utils "1.0.1-SNAPSHOT"
+  :description "FIXME: write description"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [twitter-api "0.6.0"]]
+  :profiles {:dev {:dependencies [[midje "1.4.0"]]}})
---
+++ test/twitter_utils/test/core.clj
@@ -0,0 +1,74 @@
+(ns twitter-utils.test.core
+  (:use [twitter-utils.core]
+        [midje.sweet]
+        [twitter.oauth :only [make-oauth-creds]]
+        [twitter.api.restful :only [show-followers]])
+  (:require [twitter-utils.test.fixtures :as fixtures])
+  (:import [java.util Properties]))
+
+(defn load-config-file
+  "this loads a config file from the classpath"
+  [file-name]
+  (let [file-reader (.. (Thread/currentThread)
+                        (getContextClassLoader)
+                        (getResourceAsStream file-name))
+        props (Properties.)]
+    (.load props file-reader)
+    (into {} props)))
+
+(def ^:dynamic *config* (load-config-file "test.config"))
+
+(defn assert-get
+  "get a value from the config, otherwise throw an exception detailing the problem"
+  [key-name]
+  
+  (or (get *config* key-name) 
+      (throw (Exception. (format "please define %s in the resources/test.config file" key-name)))))
+
+(def ^:dynamic *app-token* (assert-get "app.consumer.key"))
+(def ^:dynamic *app-secret* (assert-get "app.consumer.secret"))
+
+(def ^:dynamic *user-screen-name* (assert-get "user.screen.name"))
+(def ^:dynamic *user-token* (assert-get "user.access.token"))
+(def ^:dynamic *user-secret* (assert-get "user.access.token.secret"))
+
+(defn refer-private [ns] 
+     (doseq [[symbol var] (ns-interns ns)] 
+       (when (:private (meta var)) 
+         (intern *ns* symbol var))))
+
+(refer-private 'twitter-utils.core)
+
+(fact (seq-to-comma-separated [1 2 3]) => "1,2,3")
+(fact (seq-to-comma-separated #{1 2 3}) => "1,2,3")
+
+(defn seq-with-maps-with-screennames? [s]
+  (every? #(:screen_name %) s))
+
+(fact (seq-with-maps-with-screennames? [{:screen_name "Michiel"} {:screen_name "Borkdude"}]))
+
+(fact
+ "Userinfos delivers a sequence of map and every map is supposed to have a :screenname key"
+ (userinfos fixtures/idset-example-small) => seq-with-maps-with-screennames?)
+
+(defn set-of-ints? [v]
+  (and (set? v)
+       (every? integer? v)))
+
+(fact (set-of-ints? #{}))
+(fact (set-of-ints? #{1 2 3}))
+(fact (set-of-ints? nil) => false)
+(fact (set-of-ints? #{1.0 2 3}) => false)
+
+(fact (idset show-followers :params {:screen-name *user-screen-name*}) => set-of-ints?)
+
+(defn seq-of-strings? [s]
+  (every? string? s))
+
+(fact (followers-minus-friends *user-screen-name*) => seq-of-strings?)
+
+(def *creds* (make-oauth-creds *app-token* *app-secret* *user-token* *user-secret*))
+
+(fact (friend-ids-by-auth *creds*) => set-of-ints?)
+
+(fact (doesnt-follow-me-back *app-token* *app-secret* *user-token* *user-secret*) => seq-of-strings?)
